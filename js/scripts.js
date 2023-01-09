018ddba8c53869c4ff254b07fb49efcd397de634
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-01-09 15:11:28 -0800

    Finish the calculator exercise
    co-authored-by: Zachary Cipolletta
    <ZackCipolletta@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,30 @@ function add(number1, number2) {
     return number1 + number2;
   }
 
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
 
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 ;
 
-window.alert(add(number1, number2));
+/*window.alert("The addition of your two numbers is: " + add(number1, number2));
+window.alert("The subtraction of your two numbers is: " + subtract(number1, number2));
+window.alert("The multiplication of your two numbers is: " + multiply(number1, number2));
+window.alert("The division of your two numbers is: " + divide(number1, number2));*/
+
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2));
+
+window.alert(number1 + " - " + number2 + " = " + subtract(number1, number2));
+window.alert(number1 + " * " + number2 + " = " + multiply(number1, number2));
+window.alert(number1 + " / " + number2 + " = " + divide(number1, number2));
